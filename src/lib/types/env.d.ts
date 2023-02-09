f5e204180df1b05a108c0198d1209fab3907c9cd
Martin Velkov <72291444+MartsTech@users.noreply.github.com>
2023-02-09 23:25:45 +0200

    #5 | Add auth

---
+++ src/lib/types/env.d.ts
@@ -0,0 +1,8 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    GOOGLE_CLIENT_ID: string;
+    GOOGLE_CLIENT_SECRET: string;
+    NEXTAUTH_URL: string;
+    NEXTAUTH_SECRET: string;
+  }
+}
